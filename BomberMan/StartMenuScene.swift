dfa6964ede8d5ae2a37c30078c5667289b66245d
Daniel Falkedal <daniel.falkedal@gmail.com>
2022-02-17 15:56:22 +0100

    added UI Scene for UI design purpose

--- BomberMan/StartMenuScene.swift
+++ BomberMan/StartMenuScene.swift
@@ -29,7 +29,7 @@ class StartMenuScene: SKScene {
             
             if startButton.name == "startButton" {
                 
-                self.viewController?.presentScene("GameScene")
+                self.viewController?.presentScene("UIScene")
                 
             }
             
