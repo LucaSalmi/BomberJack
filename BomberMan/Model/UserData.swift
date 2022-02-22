31d78b4d9e56954cad73694a59ef8f3dfce63360
Luca Salmi <luca.salmi90@gmail.com>
2022-02-22 16:14:07 +0100

    setup

--- BomberMan/Model/UserData.swift
+++ BomberMan/Model/UserData.swift
@@ -24,5 +24,37 @@ enum UserData{
     static var numberOfDeaths: Int = 0
     static var barrelUsed: Int = 0
 
+}
+
+struct DefaultKeys{
+    
+    static let enemyKillsKey = "enemiesKilled"
+    static let bombsDroppedKey = "bombsDropped"
+    static let numbOfDeathsKey = "numberOfDeaths"
+    static let barrelUsedKey = "barrelUsed"
+}
+
+class dataReaderWriter{
+    
+    static func saveUserData(){
+        
+        let defaultData = UserDefaults.standard
+        defaultData.set(UserData.enemiesKilled, forKey: DefaultKeys.enemyKillsKey)
+        defaultData.set(UserData.bombsDropped, forKey: DefaultKeys.bombsDroppedKey)
+        defaultData.set(UserData.numberOfDeaths, forKey: DefaultKeys.numbOfDeathsKey)
+        defaultData.set(UserData.barrelUsed, forKey: DefaultKeys.barrelUsedKey)
+        
+    }
+    
+    static func loaduserData(){
+        
+        let defaultData = UserDefaults.standard
+        UserData.enemiesKilled = defaultData.integer(forKey: DefaultKeys.enemyKillsKey)
+        UserData.bombsDropped = defaultData.integer(forKey: DefaultKeys.bombsDroppedKey)
+        UserData.numberOfDeaths = defaultData.integer(forKey: DefaultKeys.numbOfDeathsKey)
+        UserData.barrelUsed = defaultData.integer(forKey: DefaultKeys.barrelUsedKey)
+    }
     
 }
+
+
--- BomberMan/Scenes and Controllers/GameScene.swift
+++ BomberMan/Scenes and Controllers/GameScene.swift
@@ -55,7 +55,6 @@ class GameScene: SKScene {
     
     override func sceneDidLoad() {
         
-        
     
     }
     
