b05d76af84238e46a15b0efdae8d48ae8224bdca
Daniel Falkedal <daniel.falkedal@gmail.com>
2022-03-15 12:09:00 +0100

    removed test line (which changed last saved level)

--- BomberMan/Scenes and Controllers/SwiftUI Interfaces/SwiftUIHostingController.swift
+++ BomberMan/Scenes and Controllers/SwiftUI Interfaces/SwiftUIHostingController.swift
@@ -39,8 +39,6 @@ struct ContentView: View {
         swiftUICommunicator = SwiftUICommunicator.instance
         
         _result = FetchRequest<Statistics>(sortDescriptors: sortingPredicate, animation: animation)
-        
-        UserData.lastSavedLevel = 4
     }
     
     var body: some View {
