7c5578c1c60b27ed381473b2e1045a6360d524df
Luca Salmi <luca.salmi90@gmail.com>
2022-03-17 11:07:44 +0100

    animation bug for rush enemy fixed

--- BomberMan/Scenes and Controllers/GameScene.swift
+++ BomberMan/Scenes and Controllers/GameScene.swift
@@ -441,14 +441,14 @@ class GameScene: SKScene {
                     player!.playerTexture.position.y = player!.position.y + PlayerSettings.textureOffset
                 }
                 
-                if tile.userData?.object(forKey: "playerCutscene") != nil {
-                    
-                    playerCutscene = PlayerCutscene()
-                    playerCutscene!.position = playerMap.centerOfTile(atColumn: column, row: row)
-                    cutsceneRunning = true
-                    addChild(playerCutscene!)
-                    
-                }
+//                if tile.userData?.object(forKey: "playerCutscene") != nil {
+//                    
+//                    playerCutscene = PlayerCutscene()
+//                    playerCutscene!.position = playerMap.centerOfTile(atColumn: column, row: row)
+//                    cutsceneRunning = true
+//                    addChild(playerCutscene!)
+//                    
+//                }
             }
         }
         
